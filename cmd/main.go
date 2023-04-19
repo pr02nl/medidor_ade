3e2b920c0bac9c1e820e6a04d9e675e5d1b77959
Paulo Braga <pr02nl@gmail.com>
2023-04-19 14:10:42 -0300

    primeiro teste lendo ade

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
-	"periph.io/x/conn/v3/physic"
-	"periph.io/x/conn/v3/spi"
-	"periph.io/x/conn/v3/spi/spireg"
+	"github.com/pr02nl/medidor_ade/pkg/ade9000"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
 	"periph.io/x/host/v3"
 )
 
@@ -14,21 +15,44 @@ func main() {
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
-	p, err := spireg.Open("")
+
+	pm_1 := gpioreg.ByName("PA7")
+	if pm_1 == nil {
+		log.Fatal("Failed to find PA7")
+	}
+	pm_1.Out(gpio.Low)
+	reset_pin := gpioreg.ByName("PA9")
+	if reset_pin == nil {
+		log.Fatal("Failed to find PA9")
+	}
+	reset_pin.Out(gpio.High)
+	resetADE9000(reset_pin)
+	time.Sleep(100 * time.Millisecond)
+
+	ade := ade9000.NewADE9000Api()
+	spi, err := ade.SPI_Init(1, "PA8")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer p.Close()
-	c, err := p.Connect(physic.MegaHertz, spi.Mode3, 8)
+	defer spi.Close()
+
+	err = ade.SetupADE9000()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Write 0x10 to the device, and read a byte right after.
-	write := []byte{0x10, 0x00}
-	read := make([]byte, len(write))
-	if err := c.Tx(write, read); err != nil {
+
+	print("Reading ADE9000 registers\n")
+	read, err := ade.SPI_Read_16bit(ade9000.ADDR_RUN)
+	if err != nil {
 		log.Fatal(err)
 	}
-	// Use read.
-	fmt.Printf("%v\n", read[1:])
+	fmt.Printf("%v\n", read)
+}
+
+func resetADE9000(reset_pin gpio.PinIO) {
+	reset_pin.Out(gpio.Low)
+	time.Sleep(50 * time.Millisecond)
+	reset_pin.Out(gpio.High)
+	time.Sleep(100 * time.Millisecond)
+	println("Reset done")
 }
