c2d4eaa47c6ba02d803a29d247e42d51a7d7166c
Paulo Braga <pr02nl@gmail.com>
2023-04-19 14:20:22 -0300

    nome corretos de portas

--- cmd/main.go
+++ cmd/main.go
@@ -11,31 +11,51 @@ import (
 	"periph.io/x/host/v3"
 )
 
+/*
+// PA8 -> CS
+// PA7 -> PM_1
+// PA9 -> ADE9000_RESET_PIN
+// PA10 -> IRQ0
+// PA20 -> IRQ1
+// PC4 -> CF4/DREADY/EVENT
+// PC7 -> CF3/ZX
+*/
+
+const (
+	CS                = "GPIO8"
+	PM_1              = "GPIO7"
+	ADE9000_RESET_PIN = "GPIO9"
+	IRQ0              = "GPIO10"
+	IRQ1              = "GPIO20"
+	CF4_DREADY_EVENT  = "GPIO68"
+	CF3_ZX            = "GPIO71"
+)
+
 func main() {
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
 
-	all := gpioreg.All()
-	for _, pin := range all {
-		fmt.Printf("Pin: %v\n", pin)
-	}
+	// all := gpioreg.All()
+	// for _, pin := range all {
+	// 	fmt.Printf("Pin: %v\n", pin)
+	// }
 
-	pm_1 := gpioreg.ByName("PA7")
+	pm_1 := gpioreg.ByName(PM_1)
 	if pm_1 == nil {
-		log.Fatal("Failed to find PA7")
+		log.Fatal("Failed to find " + PM_1)
 	}
 	pm_1.Out(gpio.Low)
-	reset_pin := gpioreg.ByName("PA9")
+	reset_pin := gpioreg.ByName(ADE9000_RESET_PIN)
 	if reset_pin == nil {
-		log.Fatal("Failed to find PA9")
+		log.Fatal("Failed to find " + ADE9000_RESET_PIN)
 	}
 	reset_pin.Out(gpio.High)
 	resetADE9000(reset_pin)
 	time.Sleep(100 * time.Millisecond)
 
 	ade := ade9000.NewADE9000Api()
-	spi, err := ade.SPI_Init(1, "PA8")
+	spi, err := ade.SPI_Init(1, CS)
 	if err != nil {
 		log.Fatal(err)
 	}
