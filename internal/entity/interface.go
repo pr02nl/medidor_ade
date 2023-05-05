7c82674461885a65e756c749070af02886408539
Paulo Braga <pr02nl@gmail.com>
2023-05-05 19:46:54 -0300

    feat: :sparkles: interfaces para repositories

---
+++ internal/entity/interface.go
@@ -0,0 +1,12 @@
+package entity
+
+type MedicaoRepositoryInterface interface {
+	Save(medicao *Medicao) error
+	InitTable() error
+}
+
+type MedidorRepositoryInterface interface {
+	Save(medidor *Medidor) error
+	InitTable() error
+	Load() (*Medidor, error)
+}
