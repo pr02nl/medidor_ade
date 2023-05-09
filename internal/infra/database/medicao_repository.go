af8d1e1625d9f747ab324516cb3524dd7bbd47c3
Paulo Braga <pr02nl@gmail.com>
2023-05-09 14:35:38 -0300

    feat: :sparkles: medidorID field

--- internal/infra/database/medicao_repository.go
+++ internal/infra/database/medicao_repository.go
@@ -16,14 +16,27 @@ func NewMedicaoRepository(db *sql.DB) *MedicaoRepository {
 
 func (r *MedicaoRepository) Save(medicao *entity.Medicao) error {
 	stmt, err := r.Db.Prepare(`
-		INSERT INTO medicao (id, datetime, voltage_rms_a, voltage_rms_b, voltage_rms_c, current_rms_a, current_rms_b, current_rms_c, current_rms_n, active_power_a, active_power_b, active_power_c, reactive_power_a, reactive_power_b, reactive_power_c, apparent_power_a, apparent_power_b, apparent_power_c, power_factor_a, power_factor_b, power_factor_c, frequency_a, frequency_b, frequency_c, angle_va_vb, angle_vb_vc, angle_va_vc, angle_va_ia, angle_vb_ib, angle_vc_ic, angle_ia_ib, angle_ib_ic, angle_ia_ic, temperature)
-		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		INSERT INTO medicao (id, medidorID, datetime, voltage_rms_a, voltage_rms_b, voltage_rms_c, 
+			current_rms_a, current_rms_b, current_rms_c, current_rms_n, active_power_a, active_power_b, 
+			active_power_c, reactive_power_a, reactive_power_b, reactive_power_c, apparent_power_a, 
+			apparent_power_b, apparent_power_c, power_factor_a, power_factor_b, power_factor_c, frequency_a, 
+			frequency_b, frequency_c, angle_va_vb, angle_vb_vc, angle_va_vc, angle_va_ia, angle_vb_ib, 
+			angle_vc_ic, angle_ia_ib, angle_ib_ic, angle_ia_ic, temperature)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 	`)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(medicao.ID, medicao.DateTime, medicao.VoltageRMS_A, medicao.VoltageRMS_B, medicao.VoltageRMS_C, medicao.CurrentRMS_A, medicao.CurrentRMS_B, medicao.CurrentRMS_C, medicao.CurrentRMS_N, medicao.ActivePower_A, medicao.ActivePower_B, medicao.ActivePower_C, medicao.ReactivePower_A, medicao.ReactivePower_B, medicao.ReactivePower_C, medicao.ApparentPower_A, medicao.ApparentPower_B, medicao.ApparentPower_C, medicao.PowerFactor_A, medicao.PowerFactor_B, medicao.PowerFactor_C, medicao.Frequency_A, medicao.Frequency_B, medicao.Frequency_C, medicao.Angle_VA_VB, medicao.Angle_VB_VC, medicao.Angle_VA_VC, medicao.Angle_VA_IA, medicao.Angle_VB_IB, medicao.Angle_VC_IC, medicao.Angle_IA_IB, medicao.Angle_IB_IC, medicao.Angle_IA_IC, medicao.Temperature)
+	_, err = stmt.Exec(medicao.ID, medicao.MedidorID, medicao.DateTime, medicao.VoltageRMS_A, medicao.VoltageRMS_B,
+		medicao.VoltageRMS_C, medicao.CurrentRMS_A, medicao.CurrentRMS_B, medicao.CurrentRMS_C,
+		medicao.CurrentRMS_N, medicao.ActivePower_A, medicao.ActivePower_B, medicao.ActivePower_C,
+		medicao.ReactivePower_A, medicao.ReactivePower_B, medicao.ReactivePower_C, medicao.ApparentPower_A,
+		medicao.ApparentPower_B, medicao.ApparentPower_C, medicao.PowerFactor_A, medicao.PowerFactor_B,
+		medicao.PowerFactor_C, medicao.Frequency_A, medicao.Frequency_B, medicao.Frequency_C,
+		medicao.Angle_VA_VB, medicao.Angle_VB_VC, medicao.Angle_VA_VC, medicao.Angle_VA_IA,
+		medicao.Angle_VB_IB, medicao.Angle_VC_IC, medicao.Angle_IA_IB, medicao.Angle_IB_IC,
+		medicao.Angle_IA_IC, medicao.Temperature)
 	if err != nil {
 		return err
 	}
@@ -34,6 +47,7 @@ func (r *MedicaoRepository) InitTable() error {
 	stmt, err := r.Db.Prepare(`
 		CREATE TABLE IF NOT EXISTS medicao (
 			id VARCHAR(36) PRIMARY KEY,
+			medidorID VARCHAR(36),
 			datetime DATETIME,
 			voltage_rms_a FLOAT,
 			voltage_rms_b FLOAT,
