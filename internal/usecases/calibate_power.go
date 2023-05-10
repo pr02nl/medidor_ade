b21a47d45253e03db5d8dc3c291cfe4542d28fa7
Paulo Braga <pr02nl@gmail.com>
2023-05-09 21:57:39 -0300

    feat: :sparkles: inicia a interrupção para a calibração

--- internal/usecases/calibate_power.go
+++ internal/usecases/calibate_power.go
@@ -1,8 +1,6 @@
 package usecases
 
 import (
-	"time"
-
 	"github.com/pr02nl/medidor_ade/internal/entity"
 	"github.com/pr02nl/medidor_ade/pkg/ade9000"
 )
@@ -19,7 +17,9 @@ func NewCalibratePowerUseCase(medidorRepository entity.MedidorRepositoryInterfac
 func (u *CalibratePowerUseCase) Execute() error {
 	println("Calibrating Power...")
 	calibration := ade9000.NewCalibration(u.ade9000)
-	time.Sleep(500 * time.Millisecond)
+	if err := calibration.CalibrationEnergyRegisterSetup(); err != nil {
+		return err
+	}
 	if err := calibration.PGain_calibrate(1); err != nil {
 		return err
 	}
